c20e1c4205dbf132843e4ce73e126cfb4551c04c
Nathan-Hutton <nathan.d.hutton@proton.me>
2025-09-17 11:24:55 -0600

    Improved ray-plane intersection code

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -9,13 +9,15 @@ bool Plane::IntersectRay(const Ray& localRay, HitInfo& hitInfo, int hitSide) con
     if (abs(localRay.dir.z) < 1e-6f) return false;
 
     const float t{ -localRay.p.z / localRay.dir.z };
+    if (t < 0.0f) return false;
+
     const Vec3f pos{ localRay.p + localRay.dir * t };
     if (abs(pos.x) > 1.0f || abs(pos.y) > 1.0f) return false;
 
     hitInfo.z = t;
     hitInfo.p = pos;
     hitInfo.N = Vec3f{ 0.0f, 0.0f, 1.0f };
-    hitInfo.front = true;
+    hitInfo.front = localRay.dir.z < 0.0f;
     return true;
 } 
 
