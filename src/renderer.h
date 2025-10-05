ddc85705ebab379f4ab010814805aeef3cacbcc8
Nathan Hutton <nathan.d.hutton@proton.me>
2025-10-05 10:15:48 -0600

    Brought back reflections/refractions

--- src/renderer.h
+++ src/renderer.h
@@ -113,7 +113,7 @@ public:
 
 	virtual Color EvalEnvironment( Vec3f const &dir ) const { return env.EvalEnvironment(dir); };	// returns the environment color
 
-	virtual bool CanBounce() const { return false; }	// returns if an additional bounce is permitted
+	virtual bool CanBounce() const { return bounceS < 5; }	// returns if an additional bounce is permitted
 	virtual int  CurrentSpecularBounce() const { return bounceS; }	// returns the current specular bounce (zero for primary rays)
 
 	// Traces a shadow ray and returns the visibility
